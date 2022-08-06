93179b716ebee7d894c76f04168893eda987c8f3
B17041425-郭家伟 <2306350174@qq.com>
2022-08-06 15:35:55 +0800

    quick setup
    setup

---
+++ Source/ThirdPersonRougelike/ThirdPersonRougelikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThirdPersonRougelikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSONROUGELIKE_API AThirdPersonRougelikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
